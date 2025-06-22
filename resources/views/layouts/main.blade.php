cf2a2b677fb772a5d457ea560b71962a30fb965b
Dmitriy <an20051806@gmail.com>
2025-06-22 15:01:06 +0300

    new styles

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,36 +6,35 @@
 	<title>@yield('title', 'Easy Tour')</title>
    				@vite('resources/css/app.css')
 </head>
-<body  style="background-image: url('{{ asset('image/bg.jpg') }}');">
+<style>
+
+</style>
+
+<body>
     <div class='main_container'>
-			<header>
-				<nav class='header__nav border-b-1'>
-					<ul class="flex flex-row gap-3 justify-center p-5 md:text-2xl text-xl font-bold">
-						<li >
-								<div class="relative inline-block text-left ">
-									<div class="group">
-											<button type="button" class="cursor-pointer p-2 hover:bg-blue-200/50 rounded-2xl transition duration-1000 ease-in-out">
-													FAQ
-											</button>
-											<div
-													class=" absolute left-0 w-40 mt-1 origin-top-left bg-white divide-y divide-gray-300 rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition duration-300">
-													<div class="py-1">
-															<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Для бизнеса</a>
-															<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">О работе сайта</a>
-													</div>
-											</div>
-									</div>
-								</div>
-						</li>
-						<li class="cursor-pointer p-2 hover:bg-blue-200/50 rounded-2xl transition duration-1000 ease-in-out"><a href="{{ route('start.index') }}">Главная</a></li>
-						<li class="cursor-pointer p-2 hover:bg-blue-200/50 rounded-2xl transition duration-1000 ease-in-out">Контакты</li>
-						<li class="cursor-pointer p-2 hover:bg-blue-200/50 rounded-2xl transition duration-1000 ease-in-out"><a href="{{ route('tours.index') }}">Туры</a></li>
-						<li class="cursor-pointer p-2 hover:bg-blue-200/50 rounded-2xl transition duration-1000 ease-in-out"><a href="{{ route('businesses.index') }}">Бизнесы</a></li>
-                        <li class="cursor-pointer p-2 hover:bg-blue-200/50 rounded-2xl transition duration-1000 ease-in-out"><a href="{{ route('businesses.index') }}">Товары 18+</a></li>
-					</ul>
-				</nav>
-			</header>
-		  @yield('content')
-	  </div>
+        <header>
+            <nav class='header__nav'>
+                <ul>
+                    <li class="relative group">
+                        <button type="button" class="flex items-center gap-1">
+                            FAQ
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <polyline points="6 9 12 15 18 9"></polyline>
+                            </svg>
+                        </button>
+                        <div class="absolute">
+                            <a href="#">Для бизнеса</a>
+                            <a href="#">О работе сайта</a>
+                        </div>
+                    </li>
+                    <li><a href="{{ route('start.index') }}">Главная</a></li>
+                    <li>Контакты</li>
+                    <li><a href="{{ route('tours.index') }}">Туры</a></li>
+                    <li><a href="{{ route('businesses.index') }}">Бизнесы</a></li>
+                </ul>
+            </nav>
+        </header>
+        @yield('content')
+    </div>
 </body>
 </html>
